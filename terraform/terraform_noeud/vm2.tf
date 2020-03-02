8b388771da36a76666c2616318a6ab67ed907fb5
aaugrain <60136082+aaugrain@users.noreply.github.com>
2020-03-02 18:30:02 +0100

    Update vm2.tf

--- terraform/terraform_noeud/vm2.tf
+++ terraform/terraform_noeud/vm2.tf
@@ -41,7 +41,7 @@ resource "azurerm_network_security_group" "NSG2" {
 resource "azurerm_network_interface" "NetI2" {
   name                = "NetI2"
   location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  resource_group_name = "${var.name}"
   network_security_group_id = "${azurerm_network_security_group.NSG2.id}"
 
   ip_configuration {
